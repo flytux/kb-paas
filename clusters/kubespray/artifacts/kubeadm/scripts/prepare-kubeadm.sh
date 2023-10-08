20480b24a8ebeb0ae869f762b814bd2a7201433e
root <jaehoon.jung@kubeworks.net>
2023-10-08 11:23:06 +0000

    update: kubespray airgapped

--- clusters/kubespray/artifacts/kubeadm/scripts/prepare-kubeadm.sh
+++ clusters/kubespray/artifacts/kubeadm/scripts/prepare-kubeadm.sh
@@ -22,7 +22,15 @@ update-ca-trust
 #module_hotfixes=1
 #EOF
 
-rpm -Uvh --force kubeadm/packages/*.rpm
+#rpm -Uvh --force kubeadm/packages/*.rpm
+yum localinstall -y kubeadm/packages/containerd.io-1.6.24-3.1.el8.x86_64.rpm
+yum localinstall -y kubeadm/packages/conntrack-tools-1.4.4-11.el8.x86_64.rpm
+yum localinstall -y kubeadm/packages/socat-1.7.4.1-1.el8.x86_64.rpm
+yum localinstall -y kubeadm/packages/iptables-1.8.4-24.el8_8.2.x86_64.rpm
+yum localinstall -y kubeadm/packages/iptables-ebtables-1.8.4-24.el8_8.2.x86_64.rpm
+yum localinstall -y kubeadm/packages/nss-3.90.0-3.el8_8.x86_64.rpm
+yum localinstall -y kubeadm/packages/ipset-7.1-1.el8.x86_64.rpm
+yum localinstall -y kubeadm/packages/bash-completion-2.7-5.el8.noarch.rpm
 
 # Install containerd
 mkdir -p /etc/containerd
@@ -37,9 +45,7 @@ cp kubeadm/kubernetes/bin/* /usr/local/bin
 chmod +x /usr/local/bin/*
 cp -R kubeadm/cni /opt
 
-# Load kubeadm container images
-nerdctl load -i kubeadm/images/kubespray-images.tar
-
+nerdctl load -i kubeadm/images/local-images.tar
 # Configure and start kubelet
 cp kubeadm/kubernetes/config/kubelet.service /etc/systemd/system
 mv kubeadm/kubernetes/config/kubelet.service.d /etc/systemd/system
--- clusters/kubespray/artifacts/scripts/run-kubespray.sh
+++ clusters/kubespray/artifacts/scripts/run-kubespray.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
+yum localinstall -y kubeadm/packages/python39-3.9.16-1.module+el8.8.0+1328+24532da6.1.x86_64.rpm
 
 python3 -m pip install kubeadm/python/*.whl 
-
-tar xf kubeadm/kubespray.tgz -C kubeadm
 
 cd kubeadm/kubespray
 
@@ -17,3 +16,5 @@ ansible-playbook -i inventory/mycluster/hosts.yaml  \
   -e kube_version=v1.26.8 -e etcd_deployment_type=kubeadm -e skip_downloads=false \
   -e local_release_dir=/root/kubeadm/kubernetes/v1.26.8 -e skip_kubeadm_images=true \
   --private-key=~/.ssh/id_rsa.key --become --become-user=root cluster.yml
+
+kubectl create -f $HOME/kubeadm/cni/calico.yaml
